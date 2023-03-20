a8f2bec0ada79a424ca5dec005ba35783c0e75bb
joaquin <benitezfloresjoaquin@gmail.com>
2023-03-20 10:11:33 +0100

    Revert "cambios varios"
    This reverts commit ac14b91604bc9d16b5c82da4a41a7fff58ae57e7.

--- tienda/forms.py
+++
@@ -1,8 +0,0 @@
-from django.forms import forms
-from tienda.models import Producto
-
-class ProductosForms(forms.ModelForm):
-    
-    class Meta:
-        model = Producto
-        fields = '__all__'
