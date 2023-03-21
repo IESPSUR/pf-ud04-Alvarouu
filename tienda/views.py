bd24c9a30b1d52412d4dfe11831293d66bc7ec99
joaquin <benitezfloresjoaquin@gmail.com>
2023-03-21 10:23:01 +0100

    view crear

--- tienda/views.py
+++ tienda/views.py
@@ -1,5 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from tienda.models import Producto
+
 
 # Create your views here.
+
+
 def welcome(request):
     return render(request,'tienda/index.html', {})
+
+
+def creaProducto(request):
+    if request.method == 'POST':
+        nombre = request.POST['nombre']
+        modelo = request.POST['modelo']
+        unidades = request.POST['unidades']
+        precio = request.POST['precio']
+        detalles = request.POST['detalles']
+        marca = request.POST['marca']
+        obj = Producto.objects.create(nombre=nombre, modelo=modelo,
+                                      unidades=unidades, precio=precio,
+                                      detalles=detalles, marca=marca)
+        obj.save()
+        return redirect('/')
